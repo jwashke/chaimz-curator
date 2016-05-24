d0c28231a1887da879dbbc8fa07dbf500833fcf2
Josh Washke <jwashke@icloud.com>
2016-05-24 10:35:32 -0600

    Add Open Struct to Artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,16 +1,7 @@
-class Artist
-
-  attr_accessor :id, :name, :created_at, :updated_at
+class Artist < OpenStruct
 
   def self.service
     ChaimzService.new
-  end
-
-  def initialize(artist_hash)
-    @id         = artist_hash[:id]
-    @name       = artist_hash[:name]
-    @created_at = artist_hash[:created_at]
-    @updated_at = artist_hash[:updated_at]
   end
 
   def self.find(id)
